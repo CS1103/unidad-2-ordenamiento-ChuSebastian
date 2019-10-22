8950f61909a3bd7a41848133366292796389830c
Sebastian Chu <45053945+ChuSebastian@users.noreply.github.com>
2019-10-22 15:54:22 -0500

    FINAL COMMIT

--- main.cpp
+++ main.cpp
@@ -16,25 +16,16 @@ using namespace std;
 
 int main() {
 
-
     vector <Record> records = VectorRecords();
-
-
-
     MaxHeap(records,false);
     ImprimirArchivo("MaxHeap1.csv",records);
     MaxHeap(records,true);
     ImprimirArchivo("MaxHeap2.csv",records);
-
     shell_sort(records);
     ImprimirArchivo("Shell1.csv",records);
-
     merge_sort(records);
     ImprimirArchivo("Merge1.csv", records);
-
     quick_sort(records,2,3);
     ImprimirArchivo("Quick1.csv",records);
-
-
     return 0;
 }
